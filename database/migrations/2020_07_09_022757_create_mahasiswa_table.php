12398447bd937ecbb8709de8b06f644559984f79
Shevrie Maulana <shevrie18@gmail.com>
2020-07-09 10:14:39 +0700

    Praktikum Pertemuan 10

---
+++ database/migrations/2020_07_09_022757_create_mahasiswa_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMahasiswaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mahasiswa', function (Blueprint $table) {
+            $table->string('nim', 13)->primary();
+            $table->string('nama_lengkap', 110);
+            $table->integer('prodi');
+            $table->text('alamat');
+            $table->string('email', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mahasiswa');
+    }
+}
